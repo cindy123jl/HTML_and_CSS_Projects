dc075e8ec445dfacd68d1b70276ebe1415e03048
Cindy <cindy123jl@gmail.com>
2020-11-30 21:47:52 -0800

    aria-required="true"

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+ alert("Hiya, world");
+ 	function My_First_Function(){
+			var string = "Kiss me, I am Irish!";
+			var result = string.fontcolor("green");
+			document.getElementById("Irish").innerhtml = result;
+}
